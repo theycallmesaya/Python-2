36cc932b0ca4584c86e9a620cdca453ba8894c31
theycallmesaya <123065528+theycallmesaya@users.noreply.github.com>
2023-09-20 14:16:40 +0600

    9correct.py

---
+++ 9correct.py
@@ -0,0 +1,26 @@
+month = int(input('Input the month (e.g. [1-12]):'))
+day = int(input('Input the day:'))
+if(month == 1):
+    print('January,', day, '.', "Season is winter")
+if(month == 2):
+    print('February,', day,'.', "Season is winter")
+if(month == 3):
+    print('March,', day,'.', "Season is spring")
+if(month == 4):
+    print('April,', day,'.', "Season is spring")
+if(month == 5):
+    print('May,', day,'.', "Season is spring")
+if(month == 6):
+    print('June,', day,'.', "Season is summer")
+if(month == 7):
+    print('July,', day,'.', "Season is summer")
+if(month == 8):
+    print('August,', day,'.', "Season is summer")
+if(month == 9):
+    print('Semtember,', day,'.', "Season is autumn")
+if(month == 10):
+    print('October,', day,'.', "Season is autumn")
+if(month == 11):
+    print('November,',  day,'.', "Season is autumn")
+if(month == 12):
+    print('December,',  day,'.', "Season is winter")
